365b0188fd146ede9e3f56143f759b613a0e18b7
MattHuang007 <144476342+MattHuang007@users.noreply.github.com>
2024-03-14 12:56:57 -0400

    w

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 
 </script>
 
@@ -10,4 +10,25 @@
 </template>
 
 <style scoped>
-</style>
+</style> -->
+<template>
+  <Doughnut :data="data" :options="options" />
+</template>
+
+<script lang="ts">
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Doughnut } from 'vue-chartjs'
+import * as chartConfig from './chartConfig.js'
+
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+export default {
+  name: 'App',
+  components: {
+    Doughnut
+  },
+  data() {
+    return chartConfig
+  }
+}
+</script>
