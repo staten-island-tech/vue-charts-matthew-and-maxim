e3b9578efb199f82dd940dab49c1ce781c628dac
MattHuang007 <144476342+MattHuang007@users.noreply.github.com>
2024-03-26 12:52:25 -0400

    Update App.vue

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -20,7 +20,40 @@ export default {
     return chartConfig
   }
 }
+
 </script>
 
 <style scoped>
 </style>
+
+<!-- <template>
+  <div class="container">
+    <Bar v-if="loaded" :data="chartData" />
+  </div>
+</template>
+
+<script>
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Doughnut } from 'vue-chartjs'
+ChartJS.register(ArcElement, Tooltip, Legend)
+export default {
+  name: 'DnutChart',
+  components: { Doughnut },
+  data: () => ({
+    loaded: false,
+    chartData: null
+  }),
+  async mounted () {
+    this.loaded = false
+
+    try {
+      const { userlist } = await fetch('https://data.cityofnewyork.us/resource/fuhs-xmg2.json')
+      this.chartdata = userlist
+
+      this.loaded = true
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
+</script> -->
