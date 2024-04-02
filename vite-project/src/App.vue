b436816a0efe1e13709ebd5e75bab25242ff5d96
MattHuang007 <144476342+MattHuang007@users.noreply.github.com>
2024-04-01 21:36:58 -0400

    Update App.vue

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,59 +1,24 @@
 <template>
-  <button @click="reloadChart('Brooklyn')">Brooklyn</button>
-  <button @click="reloadChart('Manhattan')">Manhattan</button>
-  <button @click="reloadChart('Staten Island')">Staten Island</button>
-  <button @click="reloadChart('Bronx')">Bronx</button>
-  <button @click="reloadChart('Queens')">Queens</button>
-  <button @click="reloadChart('')">All</button>
-  <div>
-    <Doughnut :data="chartData" :options="chartOptions" v-if="loaded" />
-  </div>
-
-</template>
-
-<script lang="ts">
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
-import { Doughnut } from 'vue-chartjs'
-import callApi from './api';
-
-
-async function reloadChart(borough) {
-  this.borough = borough;
-  this.loaded = false
-  try {
-    const apiData = await callApi(this.borough)
-    this.chartData = {
-      labels: Object.keys(apiData),
-      datasets: [
-        {
-          backgroundColor: ["#41B883", "#E46651", "#00D8FF", "#DD1B16", "#000000"],
-          data: Object.values(apiData),
-        },
-      ],
-    }
-
-    this.loaded = true
-  } catch (e) {
-    console.error(e)
-  }
-}
-ChartJS.register(ArcElement, Tooltip, Legend)
-
-export default {
-  name: 'App',
-  components: {
-    Doughnut
-  },
-  data: () => ({
-    loaded: false,
-    chartData: null,
-    chartOptions: {
-      responsive: true,
-      maintainAspectRatio: false,
-    },
-    borough: null
-  }),
-  async created() {
+    <button @click="reloadChart('Brooklyn')">Brooklyn</button>
+    <button @click="reloadChart('Manhattan')">Manhattan</button>
+    <button @click="reloadChart('Staten Island')">Staten Island</button>
+    <button @click="reloadChart('Bronx')">Bronx</button>
+    <button @click="reloadChart('Queens')">Queens</button>
+    <button @click="reloadChart('')">All</button>
+    <div>
+      <Doughnut :data="chartData" :options="chartOptions" v-if="loaded" />
+    </div>
+  
+  </template>
+  
+  <script lang="ts">
+  import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+  import { Doughnut } from 'vue-chartjs'
+  import callApi from './api';
+  
+  
+  async function reloadChart(borough) {
+    this.borough = borough;
     this.loaded = false
     try {
       const apiData = await callApi(this.borough)
@@ -66,20 +31,54 @@ export default {
           },
         ],
       }
-
-      console.log(this.chartData)
-
+  
       this.loaded = true
     } catch (e) {
-      alert("Failed to load chart")
       console.error(e)
     }
-  },
-  methods: { reloadChart }
-
-}
-
-</script>
-
-<style scoped></style>
-
+  }
+  ChartJS.register(ArcElement, Tooltip, Legend)
+  
+  export default {
+    name: 'App',
+    components: {
+      Doughnut
+    },
+    data: () => ({
+      loaded: false,
+      chartData: null,
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+      borough: null
+    }),
+    async created() {
+      this.loaded = false
+      try {
+        const apiData = await callApi(this.borough)
+        this.chartData = {
+          labels: Object.keys(apiData),
+          datasets: [
+            {
+              backgroundColor: ["#41B883", "#E46651", "#00D8FF", "#DD1B16", "#000000"],
+              data: Object.values(apiData),
+            },
+          ],
+        }
+  
+        console.log(this.chartData)
+  
+        this.loaded = true
+      } catch (e) {
+        alert("Failed to load chart")
+        console.error(e)
+      }
+    },
+    methods: { reloadChart }
+  
+  }
+  
+  </script>
+  
+  <style scoped></style>
